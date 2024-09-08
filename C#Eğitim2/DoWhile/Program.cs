85c7bac289b0c6a11e9115fb64abd9c6bc7aa13b
Said03393 <saideruslu0@gmail.com>
2024-09-08 19:43:26 +0300

    c2

---
+++ C#Eğitim2/DoWhile/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int number = 10;
+do // while tek farkı şart sağlanamsa bile do döngüsü bi kere çalışır
+{
+    Console.WriteLine(number);
+    number--;
+}while(number >= 0);
