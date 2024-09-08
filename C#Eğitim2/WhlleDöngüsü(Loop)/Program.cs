bfea9fb6ceaf115f92bdb3944d742d32892eb354
Said03393 <saideruslu0@gmail.com>
2024-09-08 19:37:54 +0300

    c2

--- C#Eğitim2/WhlleDöngüsü(Loop)/Program.cs
+++ C#Eğitim2/WhlleDöngüsü(Loop)/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int number = 100;
+while(number >= 0) // belirli şart sağlanana kadar döngü çalışır
+{
+    Console.WriteLine(number);
+    number--; // şarta müdahele edip belirli seviyede durdurmamız lazım -1 bürün onune geçmek icin dongu dısınde azaltma işlemi yapılır
+}
+Console.WriteLine("Sayı şuan 0 ");
