1643432bd4c668a1d81f6ed3143c3820a5281923
Said03393 <saideruslu0@gmail.com>
2024-09-08 14:32:43 +0300

    c2

--- C#Eğitim2/ifelse/Program.cs
+++ C#Eğitim2/ifelse/Program.cs
@@ -1,3 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine()
+﻿﻿// See https://aka.ms/new-console-template for more information
+
+var number = 10;
+if(number == 10) // sayı 10 oldugu süre if calısır
+{
+ Console.WriteLine("Number is 10");
+}
+
+else // if in değili else if ise ayrı bir tane daha kosul alır
+{
+    Console.WriteLine("Number is not 10");
+}
