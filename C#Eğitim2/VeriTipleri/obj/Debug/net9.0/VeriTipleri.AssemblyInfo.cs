19f81d245a1bf69edc6e95d5dcd8263b0c58ec9d
Said03393 <saideruslu0@gmail.com>
2024-09-08 14:47:37 +0300

    c2

--- C#Eğitim2/VeriTipleri/obj/Debug/net9.0/VeriTipleri.AssemblyInfo.cs
+++ C#Eğitim2/VeriTipleri/obj/Debug/net9.0/VeriTipleri.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ifelse")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("VeriTipleri")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5814fa5941d0bbbf021625cbc13488b1389a3b7a")]
-[assembly: System.Reflection.AssemblyProductAttribute("ifelse")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ifelse")]
+[assembly: System.Reflection.AssemblyProductAttribute("VeriTipleri")]
+[assembly: System.Reflection.AssemblyTitleAttribute("VeriTipleri")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
--- C#Eğitim2/ifelse/Program.cs
+++ C#Eğitim2/ifelse/Program.cs
@@ -1,12 +1,21 @@
 ﻿﻿// See https://aka.ms/new-console-template for more information
-
+var number2 = 11;
 var number = 10;
 if(number == 10) // sayı 10 oldugu süre if calısır
 {
  Console.WriteLine("Number is 10");
 }
 
+else if(number == 20)
+{
+    Console.WriteLine("Number ıs 20");
+}
+
 else // if in değili else if ise ayrı bir tane daha kosul alır
 {
     Console.WriteLine("Number is not 10");
 }
+Console.ReadLine();
+
+Console.WriteLine(number2 == 10 ? "Number is 10" : "Numner is not 10"); // sayı onsa number is 10 değilse number is not 10
+// single line if 
