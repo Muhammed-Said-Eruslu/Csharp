9d8c5df8dfaab883d7108be6bffb3645a6885633
Said03393 <saideruslu0@gmail.com>
2024-09-08 19:51:42 +0300

    c2

---
+++ C#Eğitim2/ForEachDöngüsü/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Dizi temelli dataların dolaşılması yani itareta edilmesi için kullanılır
+
+string[] students = new string[3] {"Muhammed","Said","Eruslu"};
+
+foreach(var student in students) // students için bir döngü kuruyoruz
+{
+    Console.WriteLine(student);
+}
+// foreach daha çok orneğin veri tabanından tabloyu çektiğimizde datayı çekmek için kullanır
+// üzerinde arry türünde dolaşıalbilir nesnelere karşılık gelir
