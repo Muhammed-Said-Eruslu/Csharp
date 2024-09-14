57acb7d4ab196ccc9996cfc26fc5bbdaec64a4c3
Said03393 <saideruslu0@gmail.com>
2024-09-14 14:03:53 +0300

    csharp

---
+++ C#Yeniden/ForDöngüsü/Program.cs
@@ -0,0 +1,13 @@
+﻿// 1 den 10 a kadar olan sayıları alt alta yazdırın
+
+for(int i = 0; i <= 10; i++) // kosul 0 dan 10 a kadar devam eder her basa dondugunde i yi arttırı // yanı kosul true olana kadar // dongu  basa geldiğinde ilk arttırır sonra kosulu kontrol eder 
+{
+    Console.WriteLine(i); // i nin o an kı değerini yazdırır
+}
+
+// 1 den 10 a kadar olan cıftt sayıları alt alta yazdırın
+
+for(int i = 0; i <= 10; i= i+2) // kosul 0 dan 10 a kadar devam eder her basa dondugunde i yi arttırı // yanı kosul true olana kadar // dongu  basa geldiğinde ilk arttırır sonra kosulu kontrol eder 
+{
+    Console.WriteLine(i); // i nin o an kı değerini yazdırır
+}
---
+++ C#Yeniden/İçİçeForDongusu/Program.cs
@@ -0,0 +1,9 @@
+﻿for(int i = 0; i <= 10; i++)
+{
+    for(int j = 1; j<=i; j++)
+    {
+        Console.Write(j);
+    }
+    Console.WriteLine("");
+}
+
