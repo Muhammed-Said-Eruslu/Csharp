9724b03b7b7c0e43ed6be6d33480cb6161a0e073
Said03393 <saideruslu0@gmail.com>
2024-09-14 11:22:59 +0300

    csharpp

---
+++ C#Yeniden/BedenKitleİndeksi/Program.cs
@@ -0,0 +1,29 @@
+﻿// Beden kitle indeksi = kılo / boy*boy
+// Beden kitle indeksi 18 e eşitse veya dusukse zayıf
+// 18 25 normal
+// 25 ten buyuk obez
+
+using System.Collections.Concurrent;
+
+Console.WriteLine("Kilonuzu Giriniz");
+int kılo = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Boyunuzu Girinizi");
+double boy = double.Parse(Console.ReadLine());
+
+
+
+double bki = kılo / (boy*boy);
+
+if(bki < 18)
+{
+    Console.WriteLine("Zayif");
+}
+else if(bki > 18 && bki < 25)
+{
+     Console.WriteLine("Normal");
+}
+else if(bki > 25)
+{
+    Console.WriteLine("Obez");
+}
